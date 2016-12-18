7f3d2bbf583837b5a8fcfb08d5ae4dab388472d6
Vilius Kairys <kairys.vilius@gmail.com>
2016-12-18 20:52:00 +0000

    change viewModel

--- Nhs/Program.cs
+++ Nhs/Program.cs
@@ -25,27 +25,27 @@ namespace Nhs
             Console.WriteLine($"Number of practices in London: {viewModel.PracticeCount}");
 
             Console.WriteLine("---------------------------------------------------------------");
-            Console.WriteLine("Average actual cost of all peppermint oil prescriptions: {0:C}", viewModel.AverageCost);
+            Console.WriteLine($"Average actual cost of all peppermint oil prescriptions: {viewModel.AverageCost:C}");
 
             Console.WriteLine("---------------------------------------------------------------");
             Console.WriteLine("Top-spending post codes:");
             foreach (var postCode in viewModel.PostCodeSpents)
             {
-                Console.WriteLine(string.Format(nfi, "{0,-9} {1:C}", postCode.Name, postCode.Spent));
+                Console.WriteLine(string.Format(nfi, $"{postCode.Name,-9} {postCode.Spent:C}"));
             }
 
             Console.WriteLine("---------------------------------------------------------------");
             Console.WriteLine("Average price per prescription of Flucloxacillin:");
             foreach (var region in viewModel.RegionPrescripctions)
             {
-                Console.WriteLine(string.Format(nfi, "{0,-25} {1:C} {2,8:P0}", region.Region, region.Spent, region.Diff));
+                Console.WriteLine(string.Format(nfi, $"{region.Region,-25} {region.Spent:C} {region.Diff,8:P0}"));
             }
 
             Console.WriteLine("---------------------------------------------------------------");
             Console.WriteLine("Most commonly used drug types:");
             foreach (var drugType in viewModel.DrugTypes)
             {
-                Console.WriteLine("{0,-36} {1:P0}", drugType.Name, drugType.Count);
+                Console.WriteLine($"{drugType.Name,-36} {drugType.Count:P0}");
             }
 
             Console.ReadLine();
